29e188bfcce359db7c88bd04391cd6ab571f2931
구환준/모건 <koojun99@naver.com>
2024-07-23 17:42:37 +0900

    프로젝트 시작

---
+++ src/main/java/server/wekick/WekickApplication.java
@@ -0,0 +1,13 @@
+package server.wekick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WekickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WekickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/wekick/WekickApplicationTests.java
@@ -0,0 +1,13 @@
+package server.wekick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WekickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
